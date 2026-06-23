da549eb027a971c7518b21bda0021cdf18cbbe8b
Shivendra Pradhan <pradhan.shivendra79@gmail.com>
2026-06-23 13:16:45 +0530

    add backend frontend folders and firebase config

---
+++ frontend/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD3-ZoYosyo9i3gca4G10xpAVhp-ZNjawI",
+  authDomain: "scanmarket-shiva.firebaseapp.com",
+  projectId: "scanmarket-shiva",
+  storageBucket: "scanmarket-shiva.firebasestorage.app",
+  messagingSenderId: "24340100523",
+  appId: "1:24340100523:web:46e9f8436df065f47e4eee"
+};
+
+export default firebaseConfig;
